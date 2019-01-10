b2ce3f900cdf9caa84d119a3ee7a10329b47f466
Michał <m.j.olkiewicz@gmail.com>
2019-01-11 00:01:52 +0100

    Dodany bootstrap, dodany przykladowy kontroler i widok, poczatek dodawania informacji za pomoca bazy danych, dodane obrazki, trzeba zmienic wyglad.

---
+++ RateYourEntertainment/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RateYourEntertainment.Models;
+using RateYourEntertainment.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RateYourEntertainment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IGameRepository _gameRepository;
+        public HomeController( IGameRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+        public IActionResult Index()
+        {
+            var games = _gameRepository.GetAllGames().OrderBy(g => g.Name);
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to the best website to look at rating for your favorite entertainment media. RateYourEntertainment.com",
+                Games = games.ToList()
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ RateYourEntertainment/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RateYourEntertainment.Models;
+
+namespace RateYourEntertainment.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Game> Games {get;set;}
+    }
+}
