cf3f3309438a6fb38ecc5d790645a91bf0379c24
Michał <m.j.olkiewicz@gmail.com>
2019-01-11 22:09:54 +0100

    Dodalem dane do tabeli, trzeba dodac przykladowe inputy gdy nie ma inputow.

---
+++ RateYourEntertainment/Models/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RateYourEntertainment.Models
+{
+    public static class DbInitializer
+    {
+        public static void Seed(AppDbContext context)
+        {
+            if (!context.Games.Any())
+            {
+                context.AddRange
+                    (
+                        new Game { },
+                        new Game { },
+                        new Game { },
+                        new Game { },
+                        new Game { },
+                        new Game { },
+                        new Game { },
+                        new Game { },
+                        new Game { },
+                        new Game { },
+                        new Game { }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- RateYourEntertainment/Program.cs
+++ RateYourEntertainment/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RateYourEntertainment.Models;
 
 namespace RateYourEntertainment
 {
@@ -14,11 +16,24 @@ namespace RateYourEntertainment
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            using(var scope =host.Services.CreateScope()){
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitializer.Seed(context);
+                }
+                catch (Exception)
+                {
+                    //log this in future
+                }
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+            .Build();
     }
 }
