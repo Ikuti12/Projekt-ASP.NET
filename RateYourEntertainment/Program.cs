77097ef0735ec9f8e41e8bc60c3740efa721bc20
Michał <m.j.olkiewicz@gmail.com>
2019-01-12 13:32:48 +0100

    Revert "Dodalem dane do tabeli, trzeba dodac przykladowe inputy gdy nie ma inputow."
    This reverts commit cf3f3309438a6fb38ecc5d790645a91bf0379c24.

--- RateYourEntertainment/Program.cs
+++ RateYourEntertainment/Program.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using RateYourEntertainment.Models;
 
 namespace RateYourEntertainment
 {
@@ -16,24 +14,11 @@ namespace RateYourEntertainment
     {
         public static void Main(string[] args)
         {
-            var host = BuildWebHost(args);
-            using(var scope =host.Services.CreateScope()){
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<AppDbContext>();
-                    DbInitializer.Seed(context);
-                }
-                catch (Exception)
-                {
-                    //log this in future
-                }
-            }
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-            .Build();
+                .UseStartup<Startup>();
     }
 }
